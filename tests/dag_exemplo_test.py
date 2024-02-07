27011c157b1728ee24d84fba9b895195324f26f3
marcus-moura <marcuspaulo.moura@hotmail.com>
2024-02-07 16:32:43 -0300

    fix(test): fix import dags in test files

--- tests/dag_exemplo_test.py
+++ tests/dag_exemplo_test.py
@@ -1,6 +1,6 @@
 import internal_unit_testing
 
 def test_dag_import():
-    import dag_exemplo as module
+    from dags import dag_exemplo as module
 
     internal_unit_testing.assert_has_valid_dag(module)
--- tests/dag_exemplo_test_2.py
+++ tests/dag_exemplo_test_2.py
@@ -1,6 +1,6 @@
 import internal_unit_testing
 
 def test_dag_import():
-    import dag_exemplo_2 as module
+    from dags import dag_exemplo_2 as module
 
     internal_unit_testing.assert_has_valid_dag(module)
