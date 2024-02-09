9e96427995b94c92d831dd177ca0c78aa20d7691
marcus-moura <marcuspaulo.moura@hotmail.com>
2024-02-09 16:33:30 -0300

    test: add new test

--- tests/dag_exemple_decorator_test.py
+++ tests/dag_exemple_decorator_test.py
@@ -1,6 +1,6 @@
 from dag_test_utils import unit_testing   
     
 def test_dag_import():
-    from dags import dag_exemplo as module
+    from dags import dag_exemple_decorator as module
 
     unit_testing.assert_has_valid_dag(module)
